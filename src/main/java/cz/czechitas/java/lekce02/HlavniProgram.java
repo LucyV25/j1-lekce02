fd39d16417e7b998ca7b7bd92b01cb0bc2afc99c
LucyV25 <lucievoleman@gmail.com>
2023-02-14 20:12:31 +0100

    vytvareni 4 domecku

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -6,12 +6,53 @@ public class HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
+        for (int i = 0; i < 4; i++) {
+            nakresliCtverec();
+            //dostane se na vychozi bod pro kresleni strechy
+            vychoziBodProStrechu();
+            nakresliStrechu();
+            posunSeNaDalsiDomek();
+        }
 
-        //TODO tady bude kód
+
+
     }
+
+    private void vychoziBodProStrechu() {
+        zofka.penUp();
+        zofka.turnRight(0);
+        zofka.move(100);
+    }
+
+    private void posunSeNaDalsiDomek() {
+        zofka.penUp();
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.penDown();
+    }
+
+    private void nakresliStrechu() {
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+    }
+
+    private void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
+
     }
 
 }
